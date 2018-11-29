ae76f6e0b7422b302ecfb4f9824961a8ba97888e
ermolaev <igflocal@gmail.com>
2018-11-29 14:47:32 +0700

    Change version for release 1.3.3

--- TelegramBot.php
+++ TelegramBot.php
@@ -22,7 +22,7 @@ class TelegramBotPlugin extends MantisPlugin {
         $this->name        = 'TelegramBot';
         $this->description = plugin_lang_get( 'description' );
 
-        $this->version  = '1.3.3-dev';
+        $this->version  = '1.3.3';
         $this->requires = array(
                                   'MantisCore' => '2.14.0',
         );
